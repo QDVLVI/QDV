0a84deca72035dc3bb277993a4f234a4da04f7ad
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-04-26 14:57:11 +0800

    Update startGame.cpp

--- startGame.cpp
+++ startGame.cpp
@@ -1,18 +1,11 @@
+//a_little_target.cpp
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <cstdlib>
+#include "little_target.h"
 #include "structure_define.h"
 using namespace std;
-
-//商品，to be completed
-goods good_list[8];
-goods liquor;        //白酒
-goods imported_car;  //进口汽车
-goods jade;          //玉石
-goods melon_seed;    //瓜子
-goods domian_name;   //域名
-goods gold;          //黄金
-goods copycat_phone; //山寨手机
-goods bit_coin;      //比特币
 
 int startGame(){
     role player;
@@ -23,17 +16,23 @@ int startGame(){
     if (temp == 2){
         //读取存档，to be complete,从file中获取角色信息
         //to be completed
+
+
+
     }
 
     if (temp == 1){
         //初始化角色
         cout << "Please enter your character name: " << endl;
-        char name;
+        string name;
         cin >> name;
-        player = {100000, 100000, 100, 0, 20, 70, 100};
+        player = {100000, 100000, 100, 0, 20, 70, 100, false};
 
         //建立一个文档储存数据,to be completed
         ifstream fin;
+
+
+
 
     }
 
@@ -41,16 +40,82 @@ int startGame(){
     product *storehouse;
     storehouse = new product;
 
-    //游戏主循环
+    //商品，to be completed
+    goods liquor;        //白酒
+    goods imported_car;  //进口汽车
+    goods jade;          //玉石
+    goods melon_seed;    //瓜子
+    goods domian_name;   //域名
+    goods gold;          //黄金
+    goods copycat_phone; //山寨手机
+    goods bit_coin;      //比特币
+    goods goods_list[8] = {liquor, imported_car, jade, melon_seed, domian_name, gold, copycat_phone, bit_coin};
+
+    company gaming_industry(7, 0, 0.6, 0.1, 0.02, 2, 0, 1);                //氪金游戏公司
+    company real_estate_company(40, 0, 0.8, 0.2, 0.01, 5, 0, 10);          //房地产公司
+    company electric_manufacture(5, 0, 0.6, 0.1, 0.02, 3, 0, 1);           //电器制造厂
+    company energy_drink_factory(10, 0, 0.6, 0.05, 0.03, 2, 0, 1);         //运动饮料厂
+    company internet_celebrity_anchor(0.5, 0, 0.5, 0.1, 0.01, 0, 0, 0.1);  //网红主播
+
+    company *company_list = new company[5];
+    company_list[0] = gaming_industry;
+    company_list[1] = real_estate_company;  
+    company_list[2] = electric_manufacture;
+    company_list[3] = energy_drink_factory;
+    company_list[4] = internet_celebrity_anchor;
+
+    string *company_charlist = new string[5];
+    company_charlist[0] = "gaming industry";
+    company_charlist[1] = "real estate company";
+    company_charlist[2] = "electric manufacture";
+    company_charlist[3] = "energy drink factory";
+    company_charlist[4] = "internet celebrity anchor";
+
+    bool *isSetUpCompany = new bool[5];
+    bool *isBankrupt = new bool[5];
+    for (int i = 0; i < 5; i++){
+        isSetUpCompany[i] = false;
+        isBankrupt[i] = false;
+    } 
+
     int last_year_money = 100000;
+    int market_value = 0;
+    
+    //游戏主循环
     while (player.age <= player.retire_age){
         player.age += 1;
+
+        //公司投资
+        for(int i = 0; i < 5; i++){
+            //是否破产
+            if (company_list[i].bankrupt()){
+                isBankrupt[i] = true;
+                continue;
+            }
+
+            //股价浮动
+            company_list[i].floating();
+
+            //分红,计算市值
+            if (isSetUpCompany[i] = true){
+                player.cash += company_list[i].dividend();
+                player.total_assets += company_list[i].dividend();
+                cout << "company pays dividends of " << company_list[i].dividend() << " dollar" << endl;
+            }
+        }
+
         cout << "..." << endl; // 游戏界面,从8个商品选5个出来展示，可以用file或直接cout,to be completed
+
+
+
 
         //随机事件
         if (player.total_assets > last_year_money){
             //随机奖励（年度最佳新人奖，x%触发，奖励10000-100000
             //to be completed
+
+
+
         }
 
         last_year_money = player.total_assets;
@@ -58,63 +123,102 @@ int startGame(){
         //买卖（market），创业（business）会损失健康值，
         if (player.health <= 50){
             //有几率住院
-            bool isSick; //设置一个random判断是否要住院一年, to be completed
-            if (isSick){
-                player.health = 100;
-                cout << "..." << endl; //you are sick, and u r not able to do anything this year
-                continue; //有几率认识伴侣：护士姐姐,to be completed
+            srand(time(nullptr));
+            int a = rand() % 4;
+            if (a == 1){
+                player.isSick = true;
             }
+
         }
         
-        else{
-            char input;
-            bool GoToNextYear = false;
+        //手动操作
+        char input;
+        bool GoToNextYear = false;
+
+        if (!player.isSick){
             while (cin >> input){
-                 switch (input){
+                
+                //market, 市场
+                if (input == 'm'){
+                    cout << "..." << endl; //游戏界面，写market的来设计一下按键
+                    //function, to be completed
 
-                    //market, 市场
-                    case 'm':
-                        cout << '...' << endl; //游戏界面
-                        //function, to be completed
-                        break;
+
+
+
+                }
+                
+                //business, 创业
+                else if (input == 'b'){
+                    cout << "..." << endl; //游戏界面，写business的来设计按键
+                    int cash = player.cash;
+                    business(cash, isBankrupt, isSetUpCompany, company_list, company_charlist);
                     
-                    //business, 创业
-                    case 'b':
-                        cout << '...' << endl; 
-                        //function，to be completed
-                        break;
-
-                    //spend money，花钱
-                    case 's':
-                        cout << '...' << endl;
-                        //function, to be completed
-                        break;
-
-                    //dating, 约会
-                    case 'd':
-                        cout << '...' << endl;
-                        //function, to be completed
-                        break;
-
-                    //next year
-                    case 'n':
-                        GoToNextYear = true;
-                        break;
-
-                    default:
-                        break;
                     
-                    //将更新后的数据储存到文档，to be completed
 
-                    if (GoToNextYear){
-                        break;
+                }
+
+                //spend money，花钱
+                else if (input == 's'){
+                    cout << "..." << endl; //游戏界面，写spend money的来设计界面
+                    //function, to be completed
+
+
+
+
+                }
+
+                //next year
+                else if (input == 'n'){
+                    GoToNextYear = true;
+                }
+
+                else{
+
+                }
+                        
+                player.total_assets = player.cash + market_value;  //还要再加车的价值和房的价值
+                //将更新后的数据储存到文档，to be completed
+
+                if (GoToNextYear){
+                    break;
+                }
+                
+            
+                for (int i = 0; i < 5; i++){
+                    if (isSetUpCompany[i]){
+                        market_value = company_list[i].market_value();
                     }
-                    
                 }
             }
         }
+        else{
+            cout << "you are sick this year and could not do anything this year" << endl;
+            cout << "..." << endl;     //游戏界面，按n进入下一年，按q提前退休
+
+
+
+
+            string input;
+            while(true){
+                cin >> input;
+                if (input == "n"){
+                    break;
+                }else{
+                    cout << "you are sick this year and could not do anything this year" << endl;
+                    continue;
+                }
+                
+            }
+        }
     }
+        
     
     //游戏结束，结算界面（x岁资产超百万，y岁资产超千万，z岁资产过亿，a岁资产最多为m，退休时资产为n，etc.）
-    cout << '...' << endl;
+    cout << "..." << endl;
+    delete[] company_charlist;
+    delete[] company_list;
+    delete[] isSetUpCompany;
+    delete[] isBankrupt;
+    return 0;
 }
