d7684189f0437d6a5b0e63463fc79e07d8c042a6
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-04-29 22:42:42 +0800

    Update little_target.h

--- little_target.h
+++ little_target.h
@@ -3,6 +3,6 @@
 #include "structure_define.h"
 
 //在这里declare其他函数
-void business(int &cash, bool* isBankrupt[5], bool* isSetUpCompany[5], company* company_list[5], string* company_charlist[5]);
+void business(int &cash, bool* isBankrupt, bool* isSetUpCompany, company* company_list, string* company_charlist);
 
 #endif
