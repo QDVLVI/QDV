bd0910bb1c85fd083893c6263273bc9f60c0a5ef
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-04-29 21:42:24 +0800

    Update business.cpp
    修正了部分pointer没有dereference的问题，完成了business部分的设计

--- business.cpp
+++ business.cpp
@@ -8,29 +8,32 @@
 #include "little_target.h"
 using namespace std;
 
-void business(int cash, bool isBankrupt[5], bool *isSetUpCompany[5], company *company_list[5], string *company_charlist[5]){
+void business(int cash, bool* isBankrupt[5], bool* isSetUpCompany[5], company* company_list[5], string* company_charlist[5]){
     char input;
     cout << "" << endl;     //游戏界面
 
-    bool hasSetCompany = false;
-    int temp;
-
-    for (int i = 0; i < 5; i++){
-        if (isSetUpCompany[i]){
-            hasSetCompany = true;
-            temp = i;
-        }
-    }
-
     while (true){
-        //游戏界面, to be completed
+        /*游戏界面, to be completed
+        按q退出“business”界面
+        按1-5可以对对应公司进行操作
+        若未设立公司：
+            对于未成立公司可以选择set up
+        若已设立公司
+            对于已成立且超过profit_year的公司可以选择买(b)/卖(s)
+            对于已成立但未超过profit_year的公司无法操作，无法获得分红
+            对于其他未成立公司无法操作
+*/
 
+        bool hasSetCompany = false;
+        int temp;
 
-
-
-
-
-
+        //check if player has set up a company
+        for (int i = 0; i < 5; i++){
+            if ((*isSetUpCompany)[i]){
+                hasSetCompany = true;
+                temp = i;
+            }
+        }
 
         bool quit = false;
         cin >> input;
@@ -45,42 +48,44 @@ void business(int cash, bool isBankrupt[5], bool *isSetUpCompany[5], company *co
             case '3':
             case '4':
             case '5':
-                if (!isBankrupt[input]){
-                    //已设立一家公司，可进行买/卖两种操作
+                if (!(*isBankrupt)[input]){
+                    //已设立一家公司，且过了profit year，可进行买/卖两种操作
                     if (hasSetCompany && input == temp){
+                        if (company_list[temp]->startProfit())
 
-                        cout << "..." << endl; // 游戏界面
+                            cout << "..." << endl; // 游戏界面
 
-                        cin >> input;
-                        
-                        //buy
-                        if (input == 'b'){
-                            int max_share_to_buy;
-                            max_share_to_buy = cash / (*company_list)[temp].cost_per_share;
+                            cin >> input;
+                            
+                            //buy
+                            if (input == 'b'){
+                                int max_share_to_buy;
+                                max_share_to_buy = cash / (*company_list)[temp].cost_per_share;
 
-                            cout << temp << "." << company_charlist[temp] << ":" << endl;
-                            cout << "you could buy at most" << max_share_to_buy << "shares" << endl;
-                            cout << "enter the number of shares you want to buy: " << endl;
-                            int num;
-                            cin >> num;
+                                cout << temp << "." << (*company_charlist)[temp] << ":" << endl;
+                                cout << "you could buy at most" << max_share_to_buy << "shares" << endl;
+                                cout << "enter the number of shares you want to buy: " << endl;
+                                int num;
+                                cin >> num;
 
-                            if (num >= 0 && num <= max_share_to_buy){
-                                cash -= (num * (*company_list)[temp].cost_per_share);
-                                (*company_list)[temp].average_cost = (((*company_list)[temp].average_cost * (*company_list)[temp].share_number) + (*company_list)[temp].cost_per_share * num) / ((*company_list)[temp].share_number + num);
-                                (*company_list)[temp].share_number += num;   
+                                if (num >= 0 && num <= max_share_to_buy){
+                                    cash -= (num * (*company_list)[temp].cost_per_share);
+                                    (*company_list)[temp].average_cost = (((*company_list)[temp].average_cost * (*company_list)[temp].share_number) + (*company_list)[temp].cost_per_share * num) / ((*company_list)[temp].share_number + num);
+                                    (*company_list)[temp].share_number += num;   
+                                }
+
+                                else if (num > max_share_to_buy){
+                                    cout << "you don't have enough cash" << endl;
+                                }
+
+                                else{
+                                    cout << "invalid input" << endl;
+                                }
                             }
-
-                            else if (num > max_share_to_buy){
-                                cout << "you don't have enough cash" << endl;
-                            }
-
-                            else{
-                                cout << "invalid input" << endl;
-                            }
-                        }
-                        
-                        //sell
-                        else if (input == 's'){
+                            
+                            //sell
+                            else if (input == 's'){
+                                
                             cout << "you could sell at most" << (*company_list)[temp].share_number << endl;
                             cout << "enter the number of shares you want to sell: " << endl;
                             int num;
@@ -133,6 +138,9 @@ void business(int cash, bool isBankrupt[5], bool *isSetUpCompany[5], company *co
                                 cout << "invalid input" << endl;
                             }
                         }
+                        else{
+                            cout << "your company is still developing, you can not operate on it yet" << endl;
+                        }
                     }
 
                     //已设立一家公司，对另一家公司不能再进行操作                        
@@ -142,8 +150,21 @@ void business(int cash, bool isBankrupt[5], bool *isSetUpCompany[5], company *co
 
                     //还未设立公司, 可以选择是否设立一家公司
                     else{
-                        cout << "Do you want to set up" << company_charlist[input] << endl;
-                        
+                        int temp = input;
+                        cout << "Do you want to set up" << company_charlist[temp] << endl;
+                        cout << "You need to buy 1000 shares of " <<company_charlist[temp] << "to set up this company" << endl;
+                        cin >> input;
+                        if(input == 'y'){
+                            //个人变化
+                            cash -= 1000*company_list[temp]->cost_per_share;
+                            hasSetCompany = true;
+                            *isSetUpCompany[temp] = true;
+
+                            //公司变化
+                            company_list[temp]->average_cost = company_list[temp]->cost_per_share;
+                            company_list[temp]->share_number = 1000;
+                            company_list[temp]->set_up_years = 0;    
+                        }
                         
                         // to be complete   
 
@@ -153,6 +174,7 @@ void business(int cash, bool isBankrupt[5], bool *isSetUpCompany[5], company *co
                     cout << "This company has gone bankrupt due to the recession" << endl;
                     cout << "You could not operate on this company" << endl;
                 }
+
                 break;
 
             default:
