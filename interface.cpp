9e60264fbf0a05d0fee7c86982fc8180bdc81802
daisyyyding <125641781+daisyyyding@users.noreply.github.com>
2023-05-04 20:54:52 +0800

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -22,20 +22,23 @@ int main() {
         switch (choice) {
      		case 1:
                 	std::cout << "Start Game" <<  std::endl;
-  			int result = startGame();
+  			int result = startGame();//开始游戏
                 	break;
             	case 2:
                 	std::cout << "Continue Game" <<  std::endl;
+			//根据之前存档，继续游戏
                 	break;
             	case 3:
                 	std::cout << "Talent" <<  std::endl;
-  			talent();
+  			talent();//
                 	break;
             	case 4:
                 	std::cout << "List" <<  std::endl;
+			//显示排行榜
                 	break;
      		case 5:
   			std::cout << "Help" <<  std::endl;
+			//读取readme
   			ifstream file("README.md");
   			if (file.is_open()) {
           			string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
@@ -48,6 +51,7 @@ int main() {
                 	break;
      		case 6:
   			std::cout << "Goodbye!" << std::endl;
+			//结束语待完善
                 	break;
             	default:
                 	std::cout << "Invalid choice. Please enter a number between 1 and 6." << std::endl;
