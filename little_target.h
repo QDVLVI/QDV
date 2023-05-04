1f71f3cb83da32eef5db7d5d7ec83fd22546b1e6
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-05-04 22:22:55 +0800

    Update little_target.h

--- little_target.h
+++ little_target.h
@@ -1,15 +1,13 @@
+//little_target.h
 #ifndef LITTLE_TARGET
 #define LITTLE_TARGET
 #include "structure_define.h"
 
 //在这里declare其他函数
-
-//market
 bool check(int cash,int price, int number, int capacity);
 bool select_goods(goods* good_list);
 void market(int &cash, int &health, int &storehouse_capacity, goods* good_list, map<string, store> &storehouse);
 
-//business
-void business(int &cash, bool* isBankrupt, bool* isSetUpCompany, company* company_list, string* company_charlist);
+void business(int &cash, int &health, company* company_list);
 
 #endif
