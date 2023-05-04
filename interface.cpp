c9ec33cebe307d4d3a5b741b4fa1c28cc59a0995
daisyyyding <125641781+daisyyyding@users.noreply.github.com>
2023-05-04 20:52:20 +0800

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -8,51 +8,50 @@ using namespace std;
 int main() {
     int choice;
     do {
-        cout << "100 Million Small Target" << endl;
-        cout << "1. Start Game" << endl;
-        cout << "2. Continue Game" << endl;
-        cout << "3. Talent" << endl;
-        cout << "4. List" << endl;
-	cout << "5. Help" << endl;
-	cout << "6. Exit" << endl;
-        cout << "Enter your choice (1-6): ";
+	std::cout << "===========================" << std::endl;
+        std::cout << "   100 Million Small Target   " << std::endl;
+        std::cout << "===========================" << std::endl;
+        std::cout << "1. Start Game" <<  std::endl;
+        std::cout << "2. Continue Game" <<  std::endl;
+        std::cout << "3. Talent" <<  std::endl;
+        std::cout << "4. List" <<  std::endl;
+ 	std::cout << "5. Help" <<  std::endl;
+ 	std::cout << "6. Exit" <<  std::endl;
+        std::cout << "Enter your choice (1-6): ";
         cin >> choice;
         switch (choice) {
-	    case 1:
-                cout << "You chose option 1." << endl;//
-		int result = startGame();
-                break;
-            case 2:
-                cout << "You chose option 2." << endl;
-		//待完善
-                break;
-            case 3:
-                cout << "You chose option 3." << endl;
-		talent();//这里role,player待完善
-                break;
-            case 4:
-                cout << "You chose option 4." << endl;
-		//
-                break;
-	    case 5:
-		cout << "You chose option 5." << endl;
-		ifstream file("README.md");
-		if (file.is_open()) {
-        		string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-        		cout << text << endl;
-        		file.close();
-    		}
-    		else {
-       			cout << "Unable to open file." << endl;
-    		}
-                break;
-	    case 6:
-		cout << "Goodbye!" << endl;
-		//详细文字信息，待完善
-                break;
-            default:
-                cout << "Invalid choice. Please enter a number between 1 and 6." << endl;
-                break;
+     		case 1:
+                	std::cout << "Start Game" <<  std::endl;
+  			int result = startGame();
+                	break;
+            	case 2:
+                	std::cout << "Continue Game" <<  std::endl;
+                	break;
+            	case 3:
+                	std::cout << "Talent" <<  std::endl;
+  			talent();
+                	break;
+            	case 4:
+                	std::cout << "List" <<  std::endl;
+                	break;
+     		case 5:
+  			std::cout << "Help" <<  std::endl;
+  			ifstream file("README.md");
+  			if (file.is_open()) {
+          			string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+          			cout << text << endl;
+          			file.close();
+      			}
+      			else {
+          			cout << "Unable to open file." << endl;
+      			}
+                	break;
+     		case 6:
+  			std::cout << "Goodbye!" << std::endl;
+                	break;
+            	default:
+                	std::cout << "Invalid choice. Please enter a number between 1 and 6." << std::endl;
+                	break;
         }
     } while (choice != 6);
     return 0;
