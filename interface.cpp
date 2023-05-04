4eb935dbe032869ce1c71ac6ec15004fba012eae
daisyyyding <125641781+daisyyyding@users.noreply.github.com>
2023-05-04 21:05:43 +0800

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -12,31 +12,34 @@ int main() {
         std::cout << "   100 Million Small Target   " << std::endl;
         std::cout << "===========================" << std::endl;
         std::cout << "1. Start Game" <<  std::endl;
-        std::cout << "2. Continue Game" <<  std::endl;
-        std::cout << "3. Talent" <<  std::endl;
-        std::cout << "4. List" <<  std::endl;
- 	std::cout << "5. Help" <<  std::endl;
- 	std::cout << "6. Exit" <<  std::endl;
-        std::cout << "Enter your choice (1-6): ";
+        
+        std::cout << "2. Talent" <<  std::endl;
+        std::cout << "3. List" <<  std::endl;
+ 	std::cout << "4. Help" <<  std::endl;
+ 	std::cout << "5. Exit" <<  std::endl;
+        std::cout << "Enter your choice (1-5): ";
         cin >> choice;
         switch (choice) {
      		case 1:
                 	std::cout << "Start Game" <<  std::endl;
+			
+			std::cout << "(a) New Game" <<  std::endl;
+        		std::cout << "(b) Continue Game" <<  std::endl;
+			std::cout << "Enter your choice (a/b): ";
+			char gameType;
+			cin >> gameType;
   			int result = startGame();//开始游戏
                 	break;
+            	
             	case 2:
-                	std::cout << "Continue Game" <<  std::endl;
-			//根据之前存档，继续游戏
-                	break;
-            	case 3:
                 	std::cout << "Talent" <<  std::endl;
   			talent();//
                 	break;
-            	case 4:
+            	case 3:
                 	std::cout << "List" <<  std::endl;
 			//显示排行榜
                 	break;
-     		case 5:
+     		case 4:
   			std::cout << "Help" <<  std::endl;
 			//读取readme
   			ifstream file("README.md");
@@ -49,14 +52,14 @@ int main() {
           			cout << "Unable to open file." << endl;
       			}
                 	break;
-     		case 6:
+     		case 5:
   			std::cout << "Goodbye!" << std::endl;
 			//结束语待完善
                 	break;
             	default:
-                	std::cout << "Invalid choice. Please enter a number between 1 and 6." << std::endl;
+                	std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
                 	break;
         }
-    } while (choice != 6);
+    } while (choice != 5);
     return 0;
 }
