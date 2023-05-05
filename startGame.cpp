eabe1744fa354bda83fbb35e2b67eb1a65aa5677
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-05-05 15:37:21 +0800

    Update startGame.cpp

--- startGame.cpp
+++ startGame.cpp
@@ -10,58 +10,6 @@
 
 using namespace std;
 
-void readPlayerData(role player){
-    ofstream fout;
-    fout.open("player.txt", ios::out);
-    if (fout.fail()){
-        cout << "fail to player data" << endl;
-    }else{
-        fout << player.cash << endl;
-        fout << player.total_assets << endl;
-        fout << player.health << endl;
-        fout << player.fame << endl;
-        fout << player.age << endl;
-        fout << player.retire_age << endl;
-        fout << player.storehouse_capacity << endl;
-        fout << player.isSick << endl;
-        fout.close();
-    }
-}
-
-void readCompanyData(company *company_list){
-    ofstream fout;
-    fout.open("company.txt", ios::out);
-    if (fout.fail()){
-        cout << "fail to save company data" << endl;
-    }else{
-        for (int i = 0; i < 5; i++){
-            fout << company_list[i].name << " ";
-            fout << company_list[i].cost_per_share << " ";
-            fout << company_list[i].average_cost << " ";
-            fout << company_list[i].ups_possibility << " ";
-            fout << company_list[i].amplitude << " ";
-            fout << company_list[i].dividend_rate << " ";
-            fout << company_list[i].profit_year << " ";
-            fout << company_list[i].share_number << " ";
-            fout << company_list[i].bankrupt_threshold << " ";
-            fout << company_list[i].set_up_years << " ";
-            fout << company_list[i].isSetup << " ";
-            fout << company_list[i].isBankrupt << endl;
-        }
-        fout.close();
-    }
-}
-
-void readStorehouseData(map<string, store> storehouse){
-    ofstream fout;
-    map<string, store>::iterator itr = storehouse.begin();
-    fout.open("storehouse.txt", ios::out);
-    for (itr; itr != storehouse.end(); itr++){
-        fout << itr->first << " " ;
-        fout << (itr->second).buyInPrice << " " << (itr->second).name << " " << (itr->second).number << endl;
-    }
-}
-
 int startGame(){
     //角色
     role player{100000, 100000, 100, 0, 20, 70, 100, false}; 
@@ -271,9 +219,14 @@ int startGame(){
             }
         }
            
-        player.total_assets = player.cash + market_value;  //还要再加车的价值和房的价值
-        cout << "player's total assets: " << player.total_assets << endl;
+        int good_value = 0;
+        map<string, store>::iterator itr = storehouse.begin();
+        for(itr; itr != storehouse.end(); itr++){
+            good_value += (itr->second.buyInPrice * itr->second.number);
+        }
 
+        player.total_assets = player.cash + market_value + good_value;  //还要再加车的价值和房的价值
+        cout << "player's total assets: " << player.total_assets << endl;
         readCompanyData(company_list);
         readPlayerData(player);
 
