5fd42d774b004db47b97ab61f4ea31195f150d15
Zhu Jiaying <124866010+jytiff@users.noreply.github.com>
2023-05-05 19:38:36 +0800

    Update talent.h

--- talent.h
+++ talent.h
@@ -1,1 +1,16 @@
+#ifndef TALENT
+#define TALENT
 
+//business
+extern double rate1;
+extern double rate2;
+
+//market
+extern double rate3;
+
+//spend money
+extern int ranges;
+extern int caprice;
+extern double rate4;
+
+#endif
