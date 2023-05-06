80c7872d0b7396c34f763c576ee8a50b3829d76f
GuanHaoNing <129737617+GuanHaoNing@users.noreply.github.com>
2023-05-06 18:19:06 +0800

    Update interface.cpp

--- interface.cpp
+++ interface.cpp
@@ -12,16 +12,16 @@ using namespace std;
 int main() {
     int choice;
     ifstream file("README.md");
-	ifstream fin ;
-	ofstream fout;
+	ifstream fin ("leaderboard.txt");
+	ofstream fout("leaderboard.txt");
 	vector<int> record;
 	vector<string> list;
 	string lines;
 
     do {
-		std::cout << "===============================" << std::endl;
+		std::cout << "===========================" << std::endl;
         std::cout << "   100 Million Small Target💰   " << std::endl;
-        std::cout << "===============================" << std::endl;
+        std::cout << "===========================" << std::endl;
         std::cout << "1. Start Game" <<  std::endl;
         std::cout << "2. Leaderboard" <<  std::endl;
  		std::cout << "3. Help" <<  std::endl;
@@ -31,86 +31,75 @@ int main() {
         cin >> choice;
 		int result;
 
-        switch (choice) {
-     		case 1:
-                std::cout << "Start Game" <<  std::endl;
-  				result = startGame();//开始游戏
-				if (result != -1){
-					//加入排行榜
-					
-					record.push_back(result);
-
-					int data;
-					string code;
-					fin.open("leaderboard.txt");
-					string line;
-					istringstream line_in(line);
-
-					while (getline(fin,line)){
-						line_in >> code >> data;
-						record.push_back(data);
-					}
-					
-					fin.close();
-
-					sort(record.begin(), record.end(), greater<int>());
-
-					vector<int>::iterator itr = record.begin();
-					int rank = 1;
-					fout.open("leaderboard.txt");
-					for (itr; itr != record.end(); itr++){
-						if (rank > 10){
-							break;
-						}
-						fout << rank << ": " << *itr << endl;
-						rank ++;
-					}
-					fout.close();
-				}
-                break;
-            	
-            case 2:
-            	std::cout << "Leaderboard" <<  std::endl;
-				std::cout << setw(10) << "rank" << "record" << endl;
-				fin.open("leaderboard.txt");
+		if (choice == 1){
+			std::cout << "Start Game" <<  std::endl;
+			result = startGame();//开始游戏
+			if (result != -1){
+				//加入排行榜
 				
-				while(getline(fin, lines)){
-					list.push_back(lines);
-				}
-				
-				fin.close();
+				record.push_back(result);
 
-				vector<string>::iterator stringItr = list.begin();
-				for (stringItr; stringItr != list.end(); stringItr++){
-					cout << *stringItr << endl;
-				}
-				//显示排行榜
+				int rank, data;
+				string line;
+				istringstream line_in(line);
 
-                break;
-        	case 3:
-				std::cout << "Help" <<  std::endl; 
-				//读取readme
-  			
-  				if (file.is_open()) {
-          			string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-          			cout << text << endl;
-          			file.close();
-      			}
-      			else {
-          			cout << "Unable to open file." << endl;
-      			}
-                break;
-            	
-     		case 4:
-				std::cout << "Goodbye!" << std::endl;
-				std::cout <<"Thank you for playing our game and wish you the best of luck in completing every goal in real life as well!😄"<<endl;
-                exit(0);
-				//结束语待完善
-                break;
-  			
-            default:
-            	std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
-            	break;
+				while (getline(fin,line)){
+					line_in >> rank >> data;
+					record.push_back(data);
+				}
+
+				sort(record.begin(), record.end(), greater<int>());
+
+				vector<int>::iterator itr = record.begin();
+				int rank = 1;
+				for (itr; itr != record.end(); itr++){
+					if (rank > 10){
+						break;
+					}
+					fout << rank << ": " << *itr << endl;
+					rank ++;
+				}
+			}
+		}
+			
+		else if (choice == 2){
+			std::cout << "Leaderboard" <<  std::endl;
+			std::cout << setw(10) << "rank" << "record" << endl;
+			
+			while(getline(fin, lines)){
+				list.push_back(lines);
+			}
+
+			vector<string>::iterator stringItr = list.begin();
+			for (stringItr; stringItr != list.end(); stringItr++){
+				cout << *stringItr << endl;
+			}
+			//显示排行榜
+		}
+		
+		else if (choice == 3){
+			std::cout << "Help" <<  std::endl; 
+			//读取readme
+		
+			if (file.is_open()) {
+				string text((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+				cout << text << endl;
+				file.close();
+			}
+			else {
+				cout << "Unable to open file." << endl;
+			}
+		}
+			
+		else if (choice == 4){
+			std::cout << "Goodbye!" << std::endl;
+			std::cout <<"Thank you for playing our game and wish you the best of luck in completing every goal in real life as well!😄"<<endl;
+			exit(0);
+			//结束语待完善
+		}
+		
+		else{
+			std::cout << "Invalid choice. Please enter a number between 1 and 5." << std::endl;
         }
     } while (choice != 5);
     return 0;
